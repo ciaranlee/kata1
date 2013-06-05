8fcc31ed4758fdc362f588f38d48c11a62ad181b
Ciaran Lee <ciaran.lee@gmail.com>
2013-06-05 09:18:54 +0100

    game not won if both player above 3

--- spec/tennis_spec.rb
+++ spec/tennis_spec.rb
@@ -1,12 +1,26 @@
 require_relative "../tennis_game"
 describe "something" do
+  before :each do
+    @tennis_game = TennisGame.new(:player_1, :player_2)
+  end
+
   it "creates a match" do
-    TennisGame.new(:player_1, :player_2).score.should == {:player_1 => 0, :player_2 => 0} 
+    @tennis_game.score.should == {:player_1 => 0, :player_2 => 0} 
   end
 
   it "score" do
-    tennis_game = TennisGame.new(:player_1, :player_2)
-    tennis_game.score_point(:player_1)
-    tennis_game.score.should == {:player_1 => 1, :player_2 => 0} 
+    @tennis_game.score_point(:player_1)
+    @tennis_game.score.should == {:player_1 => 1, :player_2 => 0} 
+  end
+
+  it "knows if someone wins in a simple scenario" do
+    4.times { @tennis_game.score_point(:player_1) }
+    @tennis_game.winner.should == :player_1
+  end
+
+  it "no winner if both on 40" do
+    3.times { @tennis_game.score_point(:player_1) }
+    4.times { @tennis_game.score_point(:player_2) }
+    @tennis_game.winner.should == nil
   end
 end
--- tennis_game.rb
+++ tennis_game.rb
@@ -5,9 +5,23 @@ class TennisGame
 		@score = {}
 		@score[player1] = 0
 		@score[player2] = 0
+		@player_one = player1
+		@player_two = player2
 	end
 
   def score_point(player)
     score[player] += 1
+  end
+
+  def winner
+    if score[@player_one] >= 3 && score[@player_two] >= 3
+      return nil
+    end
+
+    if(score[@player_one] == 4)
+      return @player_one
+    elsif(score[@player_two] == 4)
+      return @player_two
+    end
   end
 end
